3054edde6f983c0ccc25b2e86e394b6b8e3b7db7
Nicklas Mantek <mantek@jakota.de>
2024-06-12 15:25:34 +0200

    clean up registration

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -9,11 +9,12 @@ return [
   // Example for a module registration with Extbase controller
   'web_examples' => [
     'parent' => 'web',
-    'position' => ['after' => 'web_info'],
-    'access' => 'user',
+    'position' => "bottom",
+    'access' => 'user,group',
     'workspaces' => 'live',
     'path' => '/socialFeed',
     'labels' => 'LLL:EXT:pxa_social_feed/Resources/Private/Language/locallang_be.xlf',
+    'icon' => 'EXT:pxa_social_feed/Resources/Public/Icons/BE/feed.svg',
     // Extbase-specific configuration telling the TYPO3 Core to bootstrap Extbase
     'extensionName' => 'PxaSocialFeed',
     'controllerActions' => [
--- ext_tables.php
+++ ext_tables.php
@@ -1,33 +1,8 @@
 <?php
 
-use TYPO3\CMS\Core\Http\ApplicationType;
-use Psr\Http\Message\ServerRequestInterface;
-
 defined('TYPO3') or die();
 
 (function () {
-    if (($GLOBALS['TYPO3_REQUEST'] ?? null) instanceof ServerRequestInterface && ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()) {
-        /**
-         * Registers a Backend Module
-         */
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-            'Pixelant.pxa_social_feed',
-            'tools',     // Make module a submodule of 'tools'
-            'pxasocialfeed',    // Submodule key
-            '',                        // Position
-            [
-                \Pixelant\PxaSocialFeed\Controller\AdministrationController::class =>
-                    'index, editToken, updateToken, deleteToken, editConfiguration, ' .
-                    'updateConfiguration, deleteConfiguration, resetAccessToken, runConfiguration',
-            ],
-            [
-                'access' => 'user,group',
-                'icon' => 'EXT:pxa_social_feed/Resources/Public/Icons/BE/feed.svg',
-                'labels' => 'LLL:EXT:pxa_social_feed/Resources/Private/Language/locallang_be.xlf',
-            ]
-        );
-    }
-
     foreach (['feed', 'token', 'configuration'] as $table) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
             'tx_pxasocialfeed_domain_model_' . $table
